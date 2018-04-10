bbf3ec340ac60b44953aa9f0d1c64a30fa0a4e8c
Scott <smasherprog@gmail.com>
2018-04-09 19:51:21 -0700

    Update cpumem_monitor.h

--- cpumem_monitor.h
+++ cpumem_monitor.h
@@ -235,9 +235,10 @@ namespace NET {
             unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
 
             file = fopen("/proc/stat", "r");
-            fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow, &totalSys, &totalIdle);
-            fclose(file);
-
+            if(file){
+                fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow, &totalSys, &totalIdle);
+                fclose(file);
+            }  
             if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow || totalSys < lastTotalSys || totalIdle < lastTotalIdle) {
                 // Overflow detection. Just skip this value.
                 percent = -1.0;
